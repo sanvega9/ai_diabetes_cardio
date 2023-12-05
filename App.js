a93eaca75a8478d1f33e8cdbac544c27ae1fd022
sanvega9 <59941870+sanvega9@users.noreply.github.com>
2023-12-05 16:24:42 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text, View, ImageBackground} from 'react-native';
+
+import Screenpage from './screenapp/navigation';
+export default function App() {
+  return <Screenpage/>
+
+}
+
+
+
